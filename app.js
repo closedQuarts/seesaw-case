d537c491e16574a1e410c1eeb4b233ac06bcef5a
Efe Demirtaş <efedem21@hotmail.com>
2026-01-26 12:54:04 +0300

    app.js v0.1

---
+++ app.js
@@ -0,0 +1,29 @@
+const seesaw = document.getElementById("seesaw");
+const seesawCase =document.getElementById("seesaw-case");
+const leftWeightDisplay =document.getElementById("left-weight");
+const rightWeightDisplay =document.getElementById("right-weight");
+
+let objects = [];
+let angle = 0;
+
+function getRandomWeight(){
+    return Math.ceil(Math.random()*10);
+}
+
+seesaw.addEventListener('click',function(event){
+    const rect = seesaw.getBoundingClientRect();
+    const clickX = event.clientX - rect.left
+    const centerX = rect.width/2;
+
+    const distanceFromCenter = clickX - centerX;
+
+    console.log("Click X:",clickX);
+    console.log("Center X:",centerX);
+    console.log("Distance from center:",distanceFromCenter);
+
+    const weight = getRandomWeight();
+    console.log("Created weight: ",weight,"kg");
+
+
+
+});
